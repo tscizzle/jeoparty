02cc6b558581ddcd10b46d77ff7afca23f29a35a
Tyler S-C <tscizzle@gmail.com>
2022-01-15 18:12:58 -0500

    Tidied up, made things blank

--- server/app.py
+++ server/app.py
@@ -4,6 +4,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route("/")
+def index():
+    return "TODO: put the build/ directory's index.html here"
+
+
 @app.route("/fetch-questions")
 def get_current_time():
     return {"questions": ["hi..."]}
--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
-import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
-import _ from "lodash";
+import "./App.scss";
 
-function App() {
-  const [questions, setQuestions] = useState(0);
-
-  useEffect(() => {
-    fetch("/fetch-questions")
-      .then((res) => res.json())
-      .then((data) => {
-        setQuestions(data.questions);
-      });
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The first question is {_.first(questions)}.</p>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div className="App"></div>;
+};
 
 export default App;
