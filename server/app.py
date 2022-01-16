d7625feda00f638d2760eb5000c1b45f17374e96
Tyler S-C <tscizzle@gmail.com>
2022-01-15 19:38:21 -0500

    Connect the frontend to backend via a dummy click handler

--- server/app.py
+++ server/app.py
@@ -1,7 +1,18 @@
-import time
-from flask import Flask
+from flask import Flask, g, request
+
+import os
+
+import sqlite3
+
+STATE_DATABASE = "state.db"
+
 
 app = Flask(__name__)
+
+
+#####
+## Routes
+#####
 
 
 @app.route("/")
@@ -10,5 +21,48 @@ def index():
 
 
 @app.route("/fetch-questions")
-def get_current_time():
-    return {"questions": ["hi..."]}
+def fetch_questions():
+    return {"questions": ["He concocted the first potion.", "A hairy tomato."]}
+
+
+@app.route("/click-question", methods=["POST"])
+def click_question():
+    print(request.is_json)
+    content = request.get_json()
+    print(content)
+    return content
+
+
+#####
+## App Lifecycle
+#####
+
+
+@app.before_first_request
+def clear_state_db():
+    try:
+        print("Clearing state database...")
+        os.remove("state.db")
+        print("Cleared state database.")
+    except OSError:
+        print("No state database to clear.")
+        pass
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, "_state_database", None)
+    if db is not None:
+        db.close()
+
+
+#####
+## State Database Helpers
+#####
+
+
+def get_state_db():
+    db = getattr(g, "_state_database", None)
+    if db is None:
+        db = g._state_database = sqlite3.connect(STATE_DATABASE)
+    return db
--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
+
+import _ from "lodash";
 
 import "./App.scss";
 
-const App = () => {
-  return <div className="App"></div>;
-};
+class App extends Component {
+  state = {
+    questions: [],
+  };
+
+  /* Lifecycle methods. */
+
+  render() {
+    const { questions } = this.state;
+    return (
+      <div className="App">
+        <table>
+          <tbody>
+            {_.map(questions, (q, idx) => (
+              <tr key={idx}>
+                <td onClick={() => this.clickQuestion(q)}>{q}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    fetch("/fetch-questions")
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({ questions: res.questions });
+      });
+  }
+
+  /* Helpers. */
+
+  clickQuestion = (question) => {
+    fetch("/click-question", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({ question }),
+    });
+  };
+}
 
 export default App;
